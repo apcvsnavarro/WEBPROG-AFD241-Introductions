fdf984a78f6bfbe79601a0c63cf5a11c7e331528
Angelo <97135832+apcvsnavarro@users.noreply.github.com>
2025-04-14 10:55:10 +0800

    update

--- intro/js/myScript.js
+++ intro/js/myScript.js
@@ -1,3 +1,7 @@
 function myFunction() {
     document.getElementById("demo").innerHTML="Paragraph changed.";
+    
+    document.getElementById("p2").style.color = "blue";
+    document.getElementById("p2").style.fontFamily = "Arial";
+    document.getElementById("p2").style.fontSize = "larger";
 }
