4600dc572f19dcb86312473fa9f6a9c1c432b423
Angelo <97135832+apcvsnavarro@users.noreply.github.com>
2025-05-05 16:28:36 +0800

    Update images.js

--- intro/js/images.js
+++ intro/js/images.js
@@ -2,9 +2,9 @@ const app2 = Vue.createApp({
   data() {
    return {
      manyFoods: [
-       {name: 'Burrito', url: 'https://www.w3schools.com/vue/img_burrito.svg'},
-       {name: 'Salad', url: 'https://www.w3schools.com/vue/img_salad.svg'},
-       {name: 'Cake', url: 'https://www.w3schools.com/vue/img_cake.svg'},
+       {name: 'Burrito', url: 'https://www.facebook.com/photo/?fbid=10212654587958967&set=a.1257640060465'},
+       {name: 'Salad', url: 'https://www.facebook.com/photo/?fbid=10210668016775929&set=a.1257640060465'},
+       {name: 'Cake', url: 'https://www.facebook.com/photo/?fbid=10209838739004503&set=a.1257640060465'},
        {name: 'Soup', url: 'https://www.w3schools.com/vue/img_soup.svg'},
        {name: 'Fish', url: 'https://www.w3schools.com/vue/img_fish.svg'},
        {name: 'Pizza', url: 'https://www.w3schools.com/vue/img_pizza.svg'},
